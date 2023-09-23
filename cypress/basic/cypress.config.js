fb2d91dbf8494c2dc3404bf1904e3fa4bf001418
BlackCatto99 <145868357+BlackCatto99@users.noreply.github.com>
2023-09-23 15:08:36 -0700

    Update cypress.config.js

--- cypress/basic/cypress.config.js
+++ cypress/basic/cypress.config.js
@@ -1,15 +1,11 @@
-const { defineConfig } = require('cypress')
-const { cypressConfig } = require('@axe-core/watcher')
+    const { defineConfig } = require('cypress');
+    const { cypressConfig } = require('@axe-core/watcher');
 
-// Get your configuration from environment variables.
-const { API_KEY, SERVER_URL = 'https://axe.deque.com' } = process.env
-
-module.exports = defineConfig(
-  cypressConfig({
-    axe: {
-      apiKey: API_KEY,
-      serverURL: SERVER_URL
-    },
-    defaultCommandTimeout: 10000
-  })
-)
+    module.exports = defineConfig(
+      cypressConfig({
+        axe: {
+          apiKey: 'a92bb208-5161-468d-b631-511ebecc36cd'
+        },
+        defaultCommandTimeout: 10000
+      })
+    );
