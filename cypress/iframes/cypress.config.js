735e00cc760af3f05e1cfa26624379767c8b02aa
BlackCatto99 <145868357+BlackCatto99@users.noreply.github.com>
2023-09-23 14:05:25 -0700

    Update cypress.config.js

--- cypress/iframes/cypress.config.js
+++ cypress/iframes/cypress.config.js
@@ -4,12 +4,14 @@ const { cypressConfig } = require('@axe-core/watcher')
 // Get your configuration from environment variables.
 const { API_KEY, SERVER_URL = 'https://axe.deque.com' } = process.env
 
-module.exports = defineConfig(
+module.exports = {
+  projectId: "ay6jep",
+  defineConfig(
   cypressConfig({
     axe: {
       apiKey: 'a92bb208-5161-468d-b631-511ebecc36cd',
       serverURL: SERVER_URL
-    },
+    }},
     defaultCommandTimeout: 10000,
     video: false
   })
