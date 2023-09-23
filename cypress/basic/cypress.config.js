0da3bb2375c89719ffee4a291853c3e3f766e542
BlackCatto99 <145868357+BlackCatto99@users.noreply.github.com>
2023-09-23 13:44:20 -0700

    Update cypress.config.js

--- cypress/basic/cypress.config.js
+++ cypress/basic/cypress.config.js
@@ -7,7 +7,7 @@ const { API_KEY, SERVER_URL = 'https://axe.deque.com' } = process.env
 module.exports = defineConfig(
   cypressConfig({
     axe: {
-      apiKey: 'a92bb208-5161-468d-b631-511ebecc36cd'
+      apiKey: 'a92bb208-5161-468d-b631-511ebecc36cd',
       serverURL: SERVER_URL
     },
     defaultCommandTimeout: 10000
